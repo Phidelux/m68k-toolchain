37136c9af4fe4732d2bcbaab4f25aa4b1fbe15c1
Andreas Wilhelm <info@bornageek.com>
2017-04-02 20:34:22 +0000

    build.sh: Build steps, fixes and posix conformity

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 # Used mirrors.
 GNU_MIRROR=${GNU_MIRROR:-ftp://ftp.gnu.org/gnu}
-NEWLIB_MIRROT=${NEWLIB_MIRROR:-ftp://sourceware.org/pub}
+NEWLIB_MIRROR=${NEWLIB_MIRROR:-ftp://sourceware.org/pub}
 
 # Package versions and hashes.
 GCC_VERSION=${GCC_VERSION:-6.3.0}
@@ -33,35 +33,30 @@ MPFR_PACKAGE=mpfr-${MPFR_VERSION}.tar.xz
 MPFR_DOWNLOAD=${GNU_MIRROR}/mpfr/${MPFR_PACKAGE}
 MPFR_SHA512=${MPFR_SHA512:-3643469b9099b31e41d6ec9158196cd1c30894030c8864ee5b1b1e91b488bccbf7c263c951b03fe9f4ae6f9d29279e157a7dfed0885467d875f107a3d964f032}
 
-ftp://sourceware.org/pub/newlib/newlib-2.5.0.20170323.tar.gz
-
 NEWLIB_VERSION=${NEWLIB_VERSION:-2.5.0.}
 NEWLIB_PACKAGE=newlib-${NEWLIB_VERSION}.tar.gz
 NEWLIB_DOWNLOAD=${NEWLIB_MIRROR}/newlib/${NEWLIB_PACKAGE}
 NEWLIB_SHA512=${NEWLIB_SHA512:-4c99e8dfcb4a7ad0769b9e173ff06628d82e4993ef87d3adf9d6b5578626b14de81b4b3c5f0673ddbb49dc9f3d3628f9f8d4432dcded91f5cd3d27b7d44343cd}
 
+GDB_VERSION=${GDB_VERSION:-7.12.1}
+GDB_PACKAGE=mpc-${GDB_VERSION}.tar.xz
+GDB_DOWNLOAD=${GNU_MIRROR}/gdb/${GDB_PACKAGE}
+GDB_SHA512=${GDB_SHA512:-0ac8d0a495103611ef41167a08313a010dce6ca4c6d827cbe8558a0c1a1a8a6bfa53f1b7704251289cababbfaaf9e075550cdf741a54d6cd9ca3433d910efcd8}
+
 # Toolchain specific configuration.
-TOOLCHAIN_ROOT=/opt/m68k
+TOOLCHAIN_PREFIX=/opt/m68k
 TOOLCHAIN_WORK_DIR=/tmp/m68k-toolchain-build
 TOOLCHAIN_SOURCE_DIR=${TOOLCHAIN_WORK_DIR}/sources
 TOOLCHAIN_BUILD_DIR=${TOOLCHAIN_WORK_DIR}/build
 TOOLCHAIN_LOG_DIR=${TOOLCHAIN_WORK_DIR}/log
 
-TOOLCHAIN_HOST=$(echo "${MACHTYPE}" | sed "s/-[^-]*/-cross/")
-TOOLCHAIN_BUILD=${TOOLCHAIN_HOST}
-TOOLCHAIN_TARGET=m68k-coff
+# TOOLCHAIN_HOST=$(echo "${MACHTYPE}" | sed "s/-[^-]*/-cross/")
+# TOOLCHAIN_BUILD=${TOOLCHAIN_HOST}
+TOOLCHAIN_TARGET=m68k-elf
 
-TOOLCHAIN_CFLAGS="-O2 -pipe -fPIC"
+# TOOLCHAIN_CFLAGS="-O2 -pipe -fPIC"
 
-function pushd {
-	command pushd "$@" > /dev/null
-}
-
-function popd {
-	command popd "$@" > /dev/null
-}
-
-function download {
+download() {
 	url=$1
 	checksum=$2
 	hashmethod=$3
@@ -87,18 +82,18 @@ function download {
 	fi
 }
 
-function extract {
+extract() {
 	file=$1
 	target=$(readlink -f "$2")
 
-	if [[ ! -f "${file}" ]]; then
+	if ! [ -f "${file}" ]; then
 		echo "extract: '${file}' does not exist"
 		exit 1
 	fi
 
 	filename=$(echo basename "${file}" | awk '{print tolower($0)}')
 
-	pushd "${target}"
+	cd "${target}"
 	case "$filename" in
 		*.tar.bz2)   tar xjf "${file}"    ;;
 		*.tar.gz)    tar xzf "${file}"    ;;
@@ -119,10 +114,10 @@ function extract {
 			exit 1
 			;;
 	esac
-	popd
+	cd -
 }
 
-function downloadExtract {
+downloadExtract() {
 	url=$1
 	checksum=$2
 	hashmethod=$3
@@ -141,7 +136,7 @@ function downloadExtract {
 	extract "${TOOLCHAIN_SOURCE_DIR}/${file}" "${TOOLCHAIN_BUILD_DIR}"
 }
 
-function finish {
+finish() {
 	echo "Done."
 }
 
@@ -157,8 +152,91 @@ downloadExtract "${BINUTILS_DOWNLOAD}" "${BINUTILS_SHA512}"
 downloadExtract "${GCC_DOWNLOAD}" "${GCC_SHA512}"
 downloadExtract "${GMP_DOWNLOAD}" "${GMP_SHA512}"
 downloadExtract "${MPC_DOWNLOAD}" "${MPC_SHA512}"
-downloadExtract "${MPFR_DOWNLOAD}" "${MPFE_SHA512}"
+downloadExtract "${MPFR_DOWNLOAD}" "${MPFR_SHA512}"
 downloadExtract "${NEWLIB_DOWNLOAD}" "${NEWLIB_SHA512}"
 downloadExtract "${GDB_DOWNLOAD}" "${GDB_SHA512}"
 
-# pushd ${TOOLCHAIN_BUILD_DIR}/binutils-${BINUTILS_VERSION}
+# Build binutils
+cd "${TOOLCHAIN_BUILD_DIR}/binutils-${BINUTILS_VERSION}"
+
+mkdir build
+cd build
+
+../configure \
+	--target=${TOOLCHAIN_TARGET} \
+	--prefix=${TOOLCHAIN_PREFIX}
+
+make
+sudo make install
+
+# Move gmp, mpc and mpfr to gcc directoy.
+mv "${TOOLCHAIN_BUILD_DIR}/gmp-${GMP_VERSION}" \
+	"${TOOLCHAIN_BUILD_DIR}/gcc-${GCC_VERSION}/gmp"
+mv "${TOOLCHAIN_BUILD_DIR}/mpc-${MPC_VERSION}" \
+	"${TOOLCHAIN_BUILD_DIR}/gcc-${GCC_VERSION}/mpc"
+mv "${TOOLCHAIN_BUILD_DIR}/mpfr-${MPFR_VERSION}" \
+	"${TOOLCHAIN_BUILD_DIR}/gcc-${GCC_VERSION}/mpfr"
+
+# Build gcc.
+cd "${TOOLCHAIN_BUILD_DIR}/gcc-${GCC_VERSION}"
+
+mkdir build
+cd build
+
+../configure \
+	--target=${TOOLCHAIN_TARGET} \
+	--prefix=${TOOLCHAIN_PREFIX} \
+	--enable-languages=c \
+	--with-newlib \
+	--disable-libmudflap \
+	--disable-libssp \
+	--disable-libgomp \
+	--disable-libstdcxx-pch \
+	--disable-threads \
+	--with-gnu-as \
+	--with-gnu-ld \
+	--disable-nls \
+	--with-headers=yes \
+	--disable-checking \
+	--without-headers
+
+make -j6
+sudo make install
+
+# Build newlib.
+cd "${TOOLCHAIN_BUILD_DIR}/newlib-${NEWLIB_VERSION}"
+
+mkdir build
+cd build
+
+../configure \
+	--target=${TOOLCHAIN_TARGET} \
+	--prefix=${TOOLCHAIN_PREFIX} \
+	--enable-newlib-reent-small \
+	--disable-malloc-debugging \
+	--enable-newlib-multithread \
+	--disable-newlib-io-float \
+	--disable-newlib-supplied-syscalls \
+	--disable-newlib-io-c99-formats \
+	--disable-newlib-mb \
+	--disable-newlib-atexit-alloc \
+	--enable-target-optspace \
+	--disable-shared \
+	--enable-static \
+	--enable-fast-install
+
+make -j6
+sudo make install
+
+# Build gdb.
+cd "${TOOLCHAIN_BUILD_DIR}/gdb-${GDB_VERSION}"
+
+mkdir build
+cd build
+
+../configure \
+	--target=${TOOLCHAIN_TARGET} \
+	--prefix=${TOOLCHAIN_PREFIX} \
+
+make -j6
+sudo make install
